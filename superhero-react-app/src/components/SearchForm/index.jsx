ea15023d23b78bbc48474826fea0d765d5254952
ivshxvi <ivhsxvi@gmail.com>
2024-03-01 08:18:51 +0000

    Add SearchForm

--- superhero-react-app/src/components/SearchForm/index.jsx
+++ superhero-react-app/src/components/SearchForm/index.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useSuperhero } from '../../contexts'
+import { useEffect, useState } from 'react'
 
 const SearchForm = () => {
+    const [input, setInput] = useState('')
+    const [searchString, setSearchString ] = useState('')
+    const {superheros , setSuperheros } = useSuperhero()
+
+    useEffect(() => {
+        const SearchAPI = async () => {
+          const response = await fetch(
+            `https://superheroapi.com/api/2073755343009452/search/${searchString}`
+          );
+          const rawData = await response.json();
+          const updatedData = rawData.map((s) => s.results[0]);
+          setSuperheros(updatedData);
+        };
+    
+        SearchAPI();
+      }, [searchString]);
+
+      const handleInput = (e) => {
+        const newInput = e.target.value;
+        setInput(newInput);
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchString(input);
+        setInputValue("");
+      };
+
+    
   return (
-    <div>SearchForm</div>
+    <>
+    <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleInput} required />
+        <input type="submit" value="search" />
+    </form>
+    </>
   )
 }
 
