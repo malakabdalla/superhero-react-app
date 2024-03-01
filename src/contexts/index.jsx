df290f6f361d7e4019c2051d61f8dcea4935f9eb
malak <malookaabdalla@gmail.com>
2024-03-01 15:07:06 +0000

    photourl

--- src/contexts/index.jsx
+++ src/contexts/index.jsx
@@ -1,23 +1,30 @@
-import React, { useState, useContext, createContext } from "react";
+import { useState, useContext, createContext } from "react";
 
 const SuperheroContext = createContext();
 
+// eslint-disable-next-line react/prop-types
 export const SuperheroProvider = ({ children }) => {
-    const [superheros, setSuperheros] = useState([]);
+  const [superheros, setSuperheros] = useState([]);
 
-    const [favoriteSuperheroIds, setFavoriteSuperheroIds] = useState([]);
+  const [favoriteSuperheroIds, setFavoriteSuperheroIds] = useState([]);
 
-    const addToFavorites = (superheroId) => {
-        setFavoriteSuperheroIds((prevIds) => [...prevIds, superheroId]);
-        // You can save favoriteSuperheroIds to localStorage or any other storage mechanism here
-        localStorage.setItem("favoriteSuperheroIds", JSON.stringify([...favoriteSuperheroIds, superheroId]));
-    };
-
-    return (
-        <SuperheroContext.Provider value={{ superheros, setSuperheros, addToFavorites }}>
-            {children}
-        </SuperheroContext.Provider>
+  const addToFavorites = (superheroId) => {
+    setFavoriteSuperheroIds((prevIds) => [...prevIds, superheroId]);
+    // You can save favoriteSuperheroIds to localStorage or any other storage mechanism here
+    localStorage.setItem(
+      "favoriteSuperheroIds",
+      JSON.stringify([...favoriteSuperheroIds, superheroId])
     );
+  };
+
+  return (
+    <SuperheroContext.Provider
+      value={{ superheros, setSuperheros, addToFavorites }}
+    >
+      {children}
+    </SuperheroContext.Provider>
+  );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useSuperhero = () => useContext(SuperheroContext);
